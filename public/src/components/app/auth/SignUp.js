7526da8c0edf914fe0fdd01cc72f178b2fc56b09
jaojeda <j.a.ojeda@live.com>
2019-08-28 12:52:13 -0700

    created SignIn and SingUp components

---
+++ public/src/components/app/auth/SignUp.js
@@ -0,0 +1,44 @@
+import Component from '../../Component.js';
+
+class SignUp extends Component {
+    onRender(form) {
+        const onSignUp = this.props.onSignUp;
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const formData = new FormData(form);
+
+            const user = {
+                email: formData.get('email'),
+                password: formData.get('password')
+            };
+
+            onSignUp(user);
+        });
+    }
+
+    renderHTML() {
+        return /*html*/`
+            <form class="sign-up-form">
+   
+                <p>
+                    <label for="email">Email</label>
+                    <input id="email" type="email" name="email" required placeholder="you@somewhere.com">
+                </p>
+                
+                <p>
+                    <label for="password">Password</label>
+                    <input id="password" type="password" name="password" required>
+                </p>
+
+                <p>
+                    <button>Sign Up</button>
+                </p>
+
+            </form>
+        `;
+    }
+}
+
+export default SignUp;
